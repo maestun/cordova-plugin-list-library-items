56a97fe47ffb9079485a9663b434bc8e6f0d2243
Olivier <maestun@free.fr>
2017-08-17 10:41:12 +0200

    js trace

--- www/ListLibraryItems.js
+++ www/ListLibraryItems.js
@@ -4,22 +4,22 @@ var exec = require('cordova/exec');
 var listLibraryItems = {
 
 	isAuthorized: function(onSuccess, onError) {
-		console.log('Hello from isAuthorized');
+		console.log('ListLibraryItems plugin: calling isAuthorized...');
 		cordova.exec(onSuccess, onError, 'ListLibraryItems', 'isAuthorized', []);
 	},
 
 	requestReadAuthorization: function(onSuccess, onError, options) {
-		console.log('Hello from requestAuthorization');
+		console.log('ListLibraryItems plugin: calling requestAuthorization...');
 		cordova.exec(onSuccess, onError, 'ListLibraryItems', 'requestReadAuthorization', []);
 	},
 
 	listItems: function(includePictures, includeVideos, includeCloud, onSuccess, onError) {
-		console.log('Hello from listItems');
+		console.log('ListLibraryItems plugin: calling listItems...');
 		cordova.exec(onSuccess, onError, 'ListLibraryItems', 'listItems', [includePictures, includeVideos, includeCloud]);
 	},
 
 	uploadItem: function(payload, onSuccess, onError) {
-		console.log('Hello from uploadItem');
+		console.log('ListLibraryItems plugin: calling uploadItem...');
 		cordova.exec(onSuccess, onError, 'ListLibraryItems', 'uploadItem', [payload]);
 	}
 };
