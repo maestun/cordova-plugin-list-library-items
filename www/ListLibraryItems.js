cf1bf6cd92bea7b5df5e2b51f58aa0a137133f3a
Laurent Meunier <laurent@deltalima.net>
2019-05-17 10:44:54 +0200

    feat: Handle user selected media buckets (#23)

--- www/ListLibraryItems.js
+++ www/ListLibraryItems.js
@@ -13,14 +13,19 @@ var listLibraryItems = {
 		cordova.exec(onSuccess, onError, 'ListLibraryItems', 'requestReadAuthorization', []);
 	},
 
-	listItems: function(includePictures, includeVideos, includeCloud, onSuccess, onError) {
-		console.log('ListLibraryItems plugin: calling listItems...');
-		cordova.exec(onSuccess, onError, 'ListLibraryItems', 'listItems', [includePictures, includeVideos, includeCloud]);
+	listItems: function(includePictures, includeVideos, includeCloud, mediaBuckets, onSuccess, onError) {
+		console.log('ListLibraryItems plugin: calling listItems for buckets [%s]...', [...mediaBuckets].join(', '));
+		cordova.exec(onSuccess, onError, 'ListLibraryItems', 'listItems', [includePictures, includeVideos, includeCloud, mediaBuckets]);
 	},
 
 	uploadItem: function(payload, onSuccess, onError) {
 		console.log('ListLibraryItems plugin: calling uploadItem...');
 		cordova.exec(onSuccess, onError, 'ListLibraryItems', 'uploadItem', [payload]);
+	},
+
+	listMediaBuckets: function(onSuccess, onError) {
+		console.log('ListLibraryItems plugin: calling listMediaBuckets...');
+		cordova.exec(onSuccess, onError, 'ListLibraryItems', 'listMediaBuckets', []);
 	}
 };
 
