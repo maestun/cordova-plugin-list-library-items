d6d2215d76d4ee5b86b3d3f6b5a5965a76550a09
adrien <adrien@adriens-mbp.home>
2020-08-05 16:38:14 +0200

    fix: Added date to headers for item sync

--- src/android/io/cozy/plugins/listlibraryitems/ListLibraryItems.java
+++ src/android/io/cozy/plugins/listlibraryitems/ListLibraryItems.java
@@ -35,6 +35,7 @@ import java.net.HttpURLConnection;
 import java.net.PasswordAuthentication;
 import java.net.URL;
 import java.text.DateFormat;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
@@ -42,6 +43,7 @@ import java.util.Iterator;
 import java.util.List;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.util.TimeZone;
 
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
@@ -66,10 +68,21 @@ public class ListLibraryItems extends CordovaPlugin {
     private static final String PERMISSION_ERROR = "Permission Denial: This application is not allowed to access Photo data.";
     private SimpleDateFormat mDateFormatter = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'");
 
+    // ISO 8601 constants
+    private static final String ISO_8601_PATTERN_1 = "yyyy-MM-dd'T'HH:mm:ssZ";
+    private static final String ISO_8601_PATTERN_2 = "yyyy-MM-dd'T'HH:mm:ss.SSSZ";
+    private static final String[] SUPPORTED_ISO_8601_PATTERNS = new String[]{ISO_8601_PATTERN_1, ISO_8601_PATTERN_2};
+    private static final int TICK_MARK_COUNT = 2;
+    private static final int COLON_PREFIX_COUNT = "+00".length();
+
+    // RFC 1123 constants
+    private static final String RFC_1123_DATE_TIME = "EEE, dd MMM yyyy HH:mm:ss z";
+
     @Override
     public void initialize(CordovaInterface cordova, CordovaWebView webView) {
         super.initialize(cordova, webView);
         mContext = this.cordova.getActivity().getApplicationContext();
+        mDateFormatter.setTimeZone(TimeZone.getTimeZone("GMT"));
     }
 
     @Override
@@ -271,6 +284,36 @@ public class ListLibraryItems extends CordovaPlugin {
         return type;
     }
 
+    // This method parse a ISO8601 (non http date format) to a Date
+    public static Date parseIso8601DateTime(String string) {
+        if (string == null) {
+            return null;
+        }
+        String s = string.replace("Z", "+00:00");
+        for (String pattern : SUPPORTED_ISO_8601_PATTERNS) {
+            String str = s;
+            int colonPosition = pattern.lastIndexOf('Z') - TICK_MARK_COUNT + COLON_PREFIX_COUNT;
+            if (str.length() > colonPosition) {
+                str = str.substring(0, colonPosition) + str.substring(colonPosition + 1);
+            }
+            try {
+                return new SimpleDateFormat(pattern).parse(str);
+            } catch (final ParseException e) {
+                // try the next one
+            }
+        }
+        return null;
+    }
+
+    // This method format a Date to a RFC 1123 String
+    public static String formatRfc1123DateTime(Date date, TimeZone timeZone) {
+        SimpleDateFormat dateFormat = new SimpleDateFormat(RFC_1123_DATE_TIME);
+        if (timeZone != null) {
+            dateFormat.setTimeZone(timeZone);
+        }
+        return dateFormat.format(date);
+    }
+
     private class UploadFilePayload {
         protected CallbackContext mCallbackContext;
         protected JSONObject mJSONObject;
@@ -296,7 +339,14 @@ public class ListLibraryItems extends CordovaPlugin {
             String file_path = json.optString("filePath");
             String upload_url = json.optString("serverUrl");
             String httpMethod = json.optString("httpMethod");
+            String ISO8601_date = json.optString("createdDate");
             JSONObject headers = json.optJSONObject("headers");
+
+            // Converting date in ISO 8601 to RFC 1123
+            Date date = parseIso8601DateTime(ISO8601_date);
+            TimeZone timeZone = TimeZone.getTimeZone("GMT");
+            String RFC_1123 = formatRfc1123DateTime(date, timeZone);
+
             OkHttpClient client = new OkHttpClient();
             Call call = null;
             File thumb = null;
@@ -322,6 +372,7 @@ public class ListLibraryItems extends CordovaPlugin {
                 hb.add("Content-Length","" + FILE_SZ);
                 hb.add("User-Agent", System.getProperty("http.agent"));
                 hb.add("Expect","100-continue");
+                hb.add("Date",RFC_1123);
                 final String contentType = hb.get("Content-Type");
 
                 thumb = createThumbnail(file_path, contentType);
